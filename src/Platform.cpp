5ddcd99bd98f57bdefc61ecb50fe2e61e5948466
pigges <pigges@tutanota.com>
2024-04-19 23:58:37 +0200

    added license

---
+++ src/Platform.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pigges on 2024-04-17.
+//
+
+#include "Platform.h"
---
+++ src/Platform.h
@@ -0,0 +1,14 @@
+//
+// Created by pigges on 2024-04-17.
+//
+
+#ifndef HEALVEN_PLATFORM_H
+#define HEALVEN_PLATFORM_H
+
+
+class Platform {
+
+};
+
+
+#endif //HEALVEN_PLATFORM_H
